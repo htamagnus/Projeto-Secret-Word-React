b7fb1c8bdabc148292f491518ecda3f93cc1d2ca
hta <agathamagnusg@hotmail.com>
2022-12-14 14:09:38 -0300

    secretword

---
+++ src/data/words.js
@@ -0,0 +1,7 @@
+export const wordsList = {
+  Personagens: ["Harry", "Hermione", "Ron", "Snape", "Luna", "Minerva"],
+  Animais: ["Edwiges", "Fofo", "Bicuço", "Testrálio", "Nagini"],
+  Casas: ["Grifinória", "Sonserina", "Corvinal", "Lufalufa"],
+  Vilões: ["Lucius", "Bellatrix", "Voldemort", "Dursleys"],
+  Feitiços: ["Expelliarmus", "Estupefaça", "Crucio", "Imperio"]
+};
